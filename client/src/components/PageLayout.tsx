c3e0d2eed7d17d0fcaab536f6ba04e41de81b3fc
Weifan <li.weifa@northeastern.edu>
2026-01-09 11:51:14 -0800

    fix img load slow issue

--- client/src/components/PageLayout.tsx
+++ client/src/components/PageLayout.tsx
@@ -3,6 +3,39 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import "../index.css";
 
+// Style to ensure proper z-index layering
+const headerStyle: React.CSSProperties = {
+  position: "relative",
+};
+
+const imageStyle: React.CSSProperties = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  objectPosition: "center",
+  zIndex: 0,
+};
+
+const overlayStyle: React.CSSProperties = {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "#212529",
+  opacity: 0.5,
+  zIndex: 1,
+  pointerEvents: "none",
+};
+
+const contentStyle: React.CSSProperties = {
+  position: "relative",
+  zIndex: 2,
+};
+
 interface PageLayoutProps {
   title: string;
   subtitle: ReactNode;
@@ -21,15 +54,18 @@ const PageLayout = ({
   return (
     <div>
       <Navbar />
-      <header
-        className="masthead"
-        style={{
-          backgroundImage: `url('${backgroundImage}')`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
-        <div className="container position-relative px-4 px-lg-5">
+      <header className="masthead" style={headerStyle}>
+        {/* Background image with lazy loading */}
+        <img
+          src={backgroundImage}
+          alt=""
+          loading="lazy"
+          decoding="async"
+          style={imageStyle}
+        />
+        {/* Overlay to replace :before pseudo-element */}
+        <div style={overlayStyle} />
+        <div className="container px-4 px-lg-5" style={contentStyle}>
           <div className="row gx-4 gx-lg-5 justify-content-center">
             <div className="col-md-10 col-lg-8 col-xl-7">
               <div className="page-heading">
